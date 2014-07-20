d095175a999adbc6b3cf98562db9c4ec89c4164f
Tim Duckett <tim@timduckett.co.uk>
2014-07-20 10:05:53 +0200

    Initial Commit

---
+++ SwiftTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTable
+//
+//  Created by Tim on 20/07/14.
+//  Copyright (c) 2014 Charismatic Megafauna Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
